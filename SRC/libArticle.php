419a2d105e926298f52986b3b05845c37c4c2fd7
YamamotoManami <202410ap01_yamamoto@x-ide.sakura.n>
2025-01-17 12:33:13 +0900

    #32748
    「物件登録」画面に削除ボタンは不要

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -263,7 +263,11 @@ function subArticleEdit()
 
 		<a href="javascript:fnArticleEditCheck();"><img src="./images/<?php print $btnImage ?>" /></a>　
 		<a href="javascript:form.act.value='fManager';form.submit();"><img src="./images/btn_return.png" /></a>
-		&nbsp;&nbsp;<a href="javascript:fnArticleDeleteCheck(<?php print $articleNo ?>);"><img src="./images/btn_del.png" /></a>
+		<?php
+		if ($purpose === '更新') {
+		?>
+			&nbsp;&nbsp;<a href="javascript:fnArticleDeleteCheck(<?php print $articleNo ?>);"><img src="./images/btn_del.png" /></a>
+		<?php } ?>
 	</form>
 <?php
 }
